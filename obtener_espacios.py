d1a75d4e379a970bcfdce8ec6174c3dd2ec05433
Guma-oo <erickslonga24@gmail.com>
2026-09-25 11:19:03 -0500

    Refactor espacio selection and remove error check
    Removed check for video read failure and updated window title.

--- obtener_espacios.py
+++ obtener_espacios.py
@@ -4,21 +4,12 @@ import pickle
 video = cv2.VideoCapture('video.mp4')
 check, img = video.read()
 
-if not check:
-    print('No se pudo leer video.mp4')
-    exit()
-
 espacios = []
 
 for i in range(10):
-    espacio = cv2.selectROI('Seleccionar entradas - ENTER para guardar, C para terminar', img, False)
-    cv2.destroyWindow('Seleccionar entradas - ENTER para guardar, C para terminar')
-
-    if espacio == (0, 0, 0, 0):
-        break
-
+    espacio = cv2.selectROI('espacio', img, False)
+    cv2.destroyWindow('espacio')
     espacios.append(espacio)
-
     for x, y, w, h in espacios:
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,255), 2)
 
